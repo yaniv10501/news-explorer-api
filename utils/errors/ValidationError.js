0a2c81abe9467e6132120b3412d475d5d114a051
Yaniv Schweitzer <yaniv10501@gmail.com>
2021-12-31 03:27:56 +0200

    ValidationError fix

--- utils/errors/ValidationError.js
+++ utils/errors/ValidationError.js
@@ -1,10 +1,12 @@
 module.exports = class ValidationError extends Error {
   constructor(message) {
     super(
-      message
-        .slice(message.indexOf(':') + 2)
-        .replace(/^Path\s`\w{1,}`\s/g, '')
-        .replace(/.,/g, ',')
+      message.indexOf(':') === -1
+        ? message
+        : message
+            .slice(message.indexOf(':') + 2)
+            .replace(/^Path\s`\w{1,}`\s/g, '')
+            .replace(/.,/g, ',')
     );
     this.name = 'ValidationError';
     this.status = 400;
