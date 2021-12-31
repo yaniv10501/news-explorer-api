2433a7a255a4edbff0082f3d33419ba257bd46c8
Yaniv Schweitzer <yaniv10501@gmail.com>
2021-12-31 17:50:53 +0200

    cookie secret

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const {
   PORT = 3000,
   MONGO_DB_SERVER = 'mongodb://localhost:27017',
   NODE_ENV = 'development',
+  COOKIE_SECRET = 'cookie-secret',
 } = process.env;
 
 mongoose.connect(`${MONGO_DB_SERVER}/finalproject`);
@@ -36,7 +37,7 @@ app.set('env', NODE_ENV);
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser(COOKIE_SECRET));
 app.use(cors());
 app.options('*', cors());
 
